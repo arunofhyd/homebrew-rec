ec9f04b22a54e1dbbc5962eb4b8847ead480174f
arunofhyd <104165035+arunofhyd@users.noreply.github.com>
2026-07-14 18:56:28 +0530

    Update rec.rb

--- Casks/rec.rb
+++ Casks/rec.rb
@@ -1,8 +1,8 @@
 cask "rec" do
-  version "1.0"
-  sha256 "3a120a9165e543dfb54123f08c5d6a709f1bca7aa3b6a067210ac249fc5d622b"
+  version "1.0.0"
+  sha256 "3a120a9165e543dfb54123f08c5d6a709f1bca7aa3b6a067210ac249fc5d622b" # Make sure to put the actual shasum output here!
 
-  url "https://github.com/arunofhyd/Rec/releases/download/#{version}/Rec.zip"
+  url "https://github.com/arunofhyd/Rec/releases/download/v#{version}/Rec.zip"
   name "Rec"
   desc "Simple, clean, native-feeling screen recording"
   homepage "https://github.com/arunofhyd/Rec"
@@ -10,6 +10,6 @@ cask "rec" do
   app "Rec.app"
 
   zap trash: [
-    "~/Library/Preferences/com.local.screenrecorder.plist", # Replace with your actual bundle ID if you changed it
+    "~/Library/Preferences/com.local.screenrecorder.plist",
   ]
 end
