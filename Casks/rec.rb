99536f30174ced0ea90a8ee14aa3b88935356bbc
arunofhyd <104165035+arunofhyd@users.noreply.github.com>
2026-07-14 18:45:34 +0530

    Create rec.rb

---
+++ Casks/rec.rb
@@ -0,0 +1,15 @@
+cask "rec" do
+  version "1.0"
+  sha256 "3a120a9165e543dfb54123f08c5d6a709f1bca7aa3b6a067210ac249fc5d622b"
+
+  url "https://github.com/yourusername/Rec/releases/download/#{version}/Rec.zip"
+  name "Rec"
+  desc "Simple, clean, native-feeling screen recording"
+  homepage "https://github.com/yourusername/Rec"
+
+  app "Rec.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.local.screenrecorder.plist", # Replace with your actual bundle ID if you changed it
+  ]
+end
