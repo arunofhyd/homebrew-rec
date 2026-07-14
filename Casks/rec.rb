7ed13f353ac2364993cf3041db2eae8c6336fea6
arunofhyd <104165035+arunofhyd@users.noreply.github.com>
2026-07-14 18:49:48 +0530

    Update rec.rb

--- Casks/rec.rb
+++ Casks/rec.rb
@@ -2,10 +2,10 @@ cask "rec" do
   version "1.0"
   sha256 "3a120a9165e543dfb54123f08c5d6a709f1bca7aa3b6a067210ac249fc5d622b"
 
-  url "https://github.com/yourusername/Rec/releases/download/#{version}/Rec.zip"
+  url "https://github.com/arunofhyd/Rec/releases/download/#{version}/Rec.zip"
   name "Rec"
   desc "Simple, clean, native-feeling screen recording"
-  homepage "https://github.com/yourusername/Rec"
+  homepage "https://github.com/arunofhyd/Rec"
 
   app "Rec.app"
 
