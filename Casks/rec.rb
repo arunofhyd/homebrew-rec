36225340dbcebd40a685ec6792a15b79b428bdf8
arunofhyd <104165035+arunofhyd@users.noreply.github.com>
2026-07-16 22:11:36 +0530

    Update rec.rb

--- Casks/rec.rb
+++ Casks/rec.rb
@@ -1,6 +1,6 @@
 cask "rec" do
   version "1.0.0"
-  sha256 "3a120a9165e543dfb54123f08c5d6a709f1bca7aa3b6a067210ac249fc5d622b" # Make sure to put the actual shasum output here!
+  sha256 "91416a1d110fbbd2da91f0f9efe194c48d64f04a610471533f7288ac40388429" # Make sure to put the actual shasum output here!
 
   url "https://github.com/arunofhyd/Rec/releases/download/v#{version}/Rec.zip"
   name "Rec"
